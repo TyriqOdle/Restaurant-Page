7fa3892f85e9c380ceec3f587ae721819ab5834b
TyriqOdle <tyriqodle2005@gmail.com>
2025-05-02 08:59:40 -0400

    Deployment commit

---
+++ 006401913da6f9df6208.js
@@ -0,0 +1,24 @@
+
+import { loadMenu } from "./menuLoad.js";
+import {loadHome} from "./homePageLoad.js"
+import {loadContactUs} from "./loadContactUs.js"
+import './styles.css';
+
+
+const homeBtn = document.getElementById("homeBtn")
+const menuBtn = document.getElementById("menuBtn")
+const aboutBtn = document.getElementById("aboutBtn")
+
+homeBtn.addEventListener("click",loadHome)
+menuBtn.addEventListener("click",loadMenu)
+aboutBtn.addEventListener("click", loadContactUs)
+
+loadHome();
+
+
+
+
+
+
+
+
